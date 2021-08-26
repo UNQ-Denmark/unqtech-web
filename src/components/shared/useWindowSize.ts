ac417c5e041b0169a61fd55bd4dee3c6279e115d
soelberg484 <andreas@soelberg.biz>
2021-08-26 18:37:52 +0200

    remove console errors

--- src/components/shared/useWindowSize.ts
+++ src/components/shared/useWindowSize.ts
@@ -4,8 +4,8 @@ import debounce from 'lodash/debounce';
 import { isBrowser } from './utils';
 
 function useWindowWidth(delay = 400) {
+  if(!isBrowser()) return 1
   const [width, setWidth] = useState(window.innerWidth);
-
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
     const debouncedHandleResize = debounce(handleResize, delay);
