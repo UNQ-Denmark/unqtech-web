af93598dc049e0ad612baf0426c0437c08679154
soelberg484 <andreas@soelberg.biz>
2021-09-05 23:28:23 +0200

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: 'src/images/logo.png',
+        icon: 'src/images/favicon.png',
         author:
           'Andreas Soelberg - Linkedin: https://www.linkedin.com/in/andreas-soelberg-61276115a/',
       },
